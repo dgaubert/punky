0edf0d9a417fb25d39655c8c5ae2953f848685da
Daniel García Aubert <dgaubert@cartodb.com>
2015-11-15 02:32:50 +0100

    Missing port when starts the application

--- src/app/app.js
+++ src/app/app.js
@@ -26,7 +26,7 @@ class App {
   
   listen() {
     return new Promise((resolve, reject) => {
-      this.server = this.app.listen(1000000000);
+      this.server = this.app.listen(this.port);
       
       this.server.once('listening', () => {
         resolve(this.server);
