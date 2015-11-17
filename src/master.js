259f2c54a75d302282fc872424771facea2232fb
dgaubert <danielgarciaaubert@gmail.com>
2015-11-18 00:10:51 +0100

    Adding zero downtime reload

--- src/master.js
+++ src/master.js
@@ -8,17 +8,52 @@ class Master extends Runnable {
     this.workers = workers || process.env.WORKERS || os.cpus().length;
     this.logger = logger || console;
   }
-  
+
   run() {
     for (var i = 0; i < this.workers; i++) {
       cluster.fork();
     }
-    
+
     cluster.on('disconnect', (worker) => {
       this.logger.error('Worker %s disconnected', worker.process.pid);
     });
+
+    cluster.on('exit', function(worker, code) {
+      if(code !== 0 && !worker.suicide) {
+        this.logger.info('Worker %s crashed. Starting a new worker', worker.process.pid);
+        cluster.fork();
+      }
+    });
+
+    process.on('SIGUSR2', function() {
+      this.logger.info('Restarting workers');
+      var workers = Object.keys(cluster.workers);
+
+      function restartWorker(i) {
+        if(i >= workers.length) {
+          return;
+        }
+
+        var worker = cluster.workers[workers[i]];
+        worker.disconnect();
+
+        worker.on('exit', function() {
+          if(!worker.suicide) {
+            return;
+          }
+
+          cluster.fork()
+            .on('listening', function() {
+              restartWorker(i + 1);
+            });
+        });
+      }
+
+      restartWorker(0);
+    });
+
   }
-  
+
   exit(failure) {
     this.logger.warn('Master %s out!', process.pid);
     process.exit(failure || 0);
