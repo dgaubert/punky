5bb579a9f8d8c472606e4943040d0ff9b8d235fb
Daniel García Aubert <dgaubert@cartodb.com>
2015-11-20 16:21:27 +0100

    Renamed server property from server to httpServer

--- src/server.js
+++ src/server.js
@@ -6,23 +6,23 @@ class Server extends Runnable {
     this.app = app;
     this.port = process.env.PORT || 3000;
     this.logger = logger || console;
-    this.server = null;
+    this.httpServer = null;
   }
   
   run() {
     return new Promise((resolve, reject) => {
-      this.server = this.app.listen(this.port);
+      this.httpServer = this.app.listen(this.port);
       
-      if (!this.server) {
+      if (!this.httpServer) {
         return reject(new Error('There is not application ready yet'));
       }
       
-      this.server.once('listening', () => {
+      this.httpServer.once('listening', () => {
         this.logger.info('Application started on port', this.port);
         resolve();
       });
       
-      this.server.once('error', (err) => {
+      this.httpServer.once('error', (err) => {
         reject(err);
       });
     });
@@ -30,20 +30,20 @@ class Server extends Runnable {
   
   exit() {
     return new Promise((resolve, reject) => {
-      if (!this.server) {
+      if (!this.httpServer) {
         return reject(new Error('Application is already closed'));
       }
 
-      this.server.once('close', () => {
+      this.httpServer.once('close', () => {
         this.logger.info('Application stopped');
         resolve();
       });
       
-      this.server.once('error', (err) => {
+      this.httpServer.once('error', (err) => {
         reject(err);
       });
       
-      this.server.close();
+      this.httpServer.close();
     });
   }
 }
