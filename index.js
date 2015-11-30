4515aafd0d058c56f978bd9cd20c1ebbefcbccc1
Daniel García Aubert <dgaubert@cartodb.com>
2015-11-30 21:32:26 +0100

    Pointed to right modules

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const isMaster = require('cluster').isMaster;
-const Launcher = require('./src/launcher');
-const Master = require('./src/master');
-const Worker = require('./src/worker');
-const Server = require('./src/server');
-const App = require('./src/app/app');
+const Launcher = require('./source/launcher');
+const Master = require('./source/master');
+const Worker = require('./source/worker');
+const Server = require('./source/server');
+const App = require('./source/app/app');
 
 const target = isMaster ? new Master() : new Worker(new Server(new App()));
 const launcher = new Launcher(target);
