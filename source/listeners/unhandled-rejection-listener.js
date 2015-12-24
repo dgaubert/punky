354837863c49493e7e2b0a837a4c18cd08a31ee6
Daniel García Aubert <dgaubert@cartodb.com>
2015-12-24 11:08:59 +0100

    Added unhandled rejection listener

---
+++ source/listeners/unhandled-rejection-listener.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Listener = require('./listener');
+
+class UnhandledRejectionListener extends Listener {
+  constructor(logger) {
+    super();
+    this.logger = logger;
+  }
+
+  listen() {
+    this._listener = (reason, promise) => {
+      promise
+        .catch((err) => {
+          this.logger.error('Unhandled promise rejection:', err.stack);
+        });
+    };
+
+    process.on('unhandledRejection', this._listener);
+  }
+
+}
+
+module.exports = UnhandledRejectionListener;
