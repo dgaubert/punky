9a37d2bbafc77ffd870e48d83dbe30b123e97f6b
Daniel García Aubert <dgaubert@cartodb.com>
2015-12-27 19:57:35 +0100

    Fix typo

--- source/logging/winston-console-transport.js
+++ source/logging/winston-console-transport.js
@@ -1,16 +1,20 @@
 'use strict';
 
 const winston = require('winston');
+const colorize = require('winston').config.colorize;
 const ConsoleTransport =  require('winston').transports.Console;
 
 class WinstonConsole {
   static create() {
     return new ConsoleTransport({
       formatter: function (options) {
-        var colorize = winston.config.colorize;
         var level = colorize(options.level, options.level.toUpperCase());
 
-        return level + ': ' + new Date().toISOString() + ' - ' + options.meta.role + ' ' + options.meta.pid + ' - ' + options.message;
+        return level + ': ' +
+            new Date().toISOString() + ' - ' +
+            options.meta.role + ' ' +
+            options.meta.pid + ' - ' +
+            options.message;
       }
     });
   }
