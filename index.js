04536aaf4899bb28198edaf10f406225f5437315
Daniel García Aubert <dgaubert@cartodb.com>
2016-03-16 00:01:14 +0100

    Added minimist dependency to handle process arguments. Now server port is passed by process arguments and injected properly to server

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 'use strict'
 
+const minimist = require('minimist')
 const ServiceFactory = require('./lib/service-factory')
+
+var options = minimist(process.argv.slice(2), {
+  alias: {
+    c: 'cluster',
+    p: 'port',
+    v: 'version'
+  },
+  boolean: [ // options that are always boolean
+    'cluster',
+  ],
+  default: {
+    port: 3000
+  }
+})
+
 const serviceFactory = new ServiceFactory()
-const service = serviceFactory.create()
+const service = serviceFactory.create(options)
 
 service.run()
