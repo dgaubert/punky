3d0219654aaab073542ce09c15f471e52fdbf5f6
Daniel García Aubert <dgaubert@cartodb.com>
2016-03-19 17:28:16 +0100

    Now command line arguments are handle by argument-parser using minimist

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 #!/usr/bin/env node
 'use strict'
 
-const minimist = require('minimist')
 const ServiceFactory = require('./lib/service-factory')
 
-var options = minimist(process.argv.slice(2), {
-  alias: {
-    c: 'cluster',
-    p: 'port',
-    v: 'version'
-  },
-  boolean: [ // options that are always boolean
-    'cluster'
-  ],
-  default: {
-    cluster: false,
-    port: 3000
-  }
-})
-
 const serviceFactory = new ServiceFactory()
-const service = serviceFactory.create(options.cluster, options.port)
+const argv = process.argv.slice(2);
+const service = serviceFactory.create(argv)
 
 service.run()
