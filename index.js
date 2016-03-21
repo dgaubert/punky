9a138c1ee825f5073cef80121d494cc687fb2895
Daniel García Aubert <dgaubert@cartodb.com>
2016-03-21 23:45:51 +0100

    Added unit test for argument parser

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 'use strict'
 
 const ServiceFactory = require('./lib/service-factory')
+const ArgumentParser = require('.lib//argv/argument-parser')
 
 const serviceFactory = new ServiceFactory()
-const argv = process.argv.slice(2)
-const service = serviceFactory.create(argv)
+
+const processArguments = process.argv.slice(2)
+const argumentParser = new ArgumentParser()
+const options = argumentParser.parse(processArguments)
+
+const service = serviceFactory.create(options)
 
 service.run()
