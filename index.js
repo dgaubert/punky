27796462157a89247882bece88ccf56a256971ec
Daniel García Aubert <dgaubert@cartodb.com>
2016-04-03 16:01:47 +0200

    Exposed logger to be used by dependants

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 'use strict'
 
 const ServiceFactory = require('./lib/service-factory')
+const LoggerFactory = require('./logger/logger-factory')
+const loggerFactory = new LoggerFactory()
+const logger = loggerFactory.create()
 
 module.exports = function (router) {
   const serviceFactory = new ServiceFactory()
-  return serviceFactory.create(router)
-};
+
+  return serviceFactory.create(router, logger)
+}
+
+module.exports.logger = logger
