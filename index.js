e29343f31024da56e5434d47d4a8893b20bbc073
Daniel García Aubert <dgaubert@cartodb.com>
2016-04-03 15:45:34 +0200

    Made module as library

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-#!/usr/bin/env node
 'use strict'
 
 const ServiceFactory = require('./lib/service-factory')
-const ArgumentParser = require('./lib/argv/argument-parser')
 
-const serviceFactory = new ServiceFactory()
-
-const processArguments = process.argv.slice(2)
-const argumentParser = new ArgumentParser()
-const options = argumentParser.parse(processArguments)
-
-const service = serviceFactory.create(options)
-
-service.run()
+module.exports = function (router) {
+  const serviceFactory = new ServiceFactory()
+  return serviceFactory.create(router)
+};
