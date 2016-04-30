7647acb479c3a796fdce01b242b2bb0c4adbe9aa
Daniel García Aubert <dgaubert@cartodb.com>
2016-04-30 02:27:21 +0200

    Made service as library, role injectoed to logger and fixed minor bugs

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 'use strict'
 
-const ServiceFactory = require('./lib/service-factory')
-const LoggerFactory = require('./logger/logger-factory')
-const loggerFactory = new LoggerFactory()
-const logger = loggerFactory.create()
-
-module.exports = function (router) {
-  const serviceFactory = new ServiceFactory()
-
-  return serviceFactory.create(router, logger)
-}
-
-module.exports.logger = logger
+module.exports = require('./lib/service-factory')
