ba9ceccfc4cb147bf7aef599ec580f06737bea72
Daniel García Aubert <dgaubert@cartodb.com>
2016-05-07 02:02:27 +0200

    Improved test for punky module

--- test/integration/punky.js
+++ test/integration/punky.js
@@ -4,9 +4,12 @@ const RunnerInterface = require(__source + 'runner-interface')
 const Punky = require(__source + 'punky')
 
 describe('punky', () => {
+  beforeEach(() => {
+    this.punky = new Punky()
+  })
+
   it('.use() should return a Runner instance', () => {
-    const punky = new Punky()
-    const service = punky.use()
+    const service = this.punky.use()
 
     service.should.be.instanceOf(RunnerInterface)
   })
