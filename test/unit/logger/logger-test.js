13113f0e601dee87c750c8b5bff6cef17ba11dd8
Daniel García Aubert <dgaubert@cartodb.com>
2016-05-07 21:26:14 +0200

    Added unit test for logger

---
+++ test/unit/logger/logger-test.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const sinon = require('sinon')
+const LoggerInterface = require(__source + 'logger-interface')
+const Logger = require(__source + 'logger/logger')
+
+describe('logger', () => {
+  beforeEach(() => {
+    this.sandbox = sinon.sandbox.create()
+
+    this._logger = new LoggerInterface()
+    this._logger.rewriters = { push: () => {} }
+    this.rewritesrPushStub = this.sandbox.stub(this._logger.rewriters, 'push')
+
+    this.logger = new Logger(this._logger, 'wadus')
+  })
+
+  afterEach(() => {
+    this.rewritesrPushStub.calledOnce.should.equal(true)
+    this.sandbox.restore()
+  })
+
+  it('.log() should log', () => {
+    const loggerLogStub = this.sandbox.stub(this._logger, 'log')
+    const args = [ 'info', 'wadus message' ]
+
+    this.logger.log(...args)
+
+    loggerLogStub.calledWithExactly(...args).should.equal(true)
+  })
+
+  it('.info() should log at info level', () => {
+    const loggerInfoStub = this.sandbox.stub(this._logger, 'info')
+    const args = [ 'wadus message' ]
+
+    this.logger.info(...args)
+
+    loggerInfoStub.calledWithExactly(...args).should.equal(true)
+  })
+
+  it('.warn() should log at warn level', () => {
+    const loggerWarnStub = this.sandbox.stub(this._logger, 'warn')
+    const args = [ 'wadus message' ]
+
+    this.logger.warn(...args)
+
+    loggerWarnStub.calledWithExactly(...args).should.equal(true)
+  })
+
+  it('.error() should log at error level', () => {
+    const loggerErrorStub = this.sandbox.stub(this._logger, 'error')
+    const args = [ 'wadus message' ]
+
+    this.logger.error(...args)
+
+    loggerErrorStub.calledWithExactly(...args).should.equal(true)
+  })
+})
