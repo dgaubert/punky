742e8de7df413992019510630471a005ac3dc4cc
Daniel García Aubert <dgaubert@cartodb.com>
2016-09-11 13:07:11 +0200

    Improved punky startup, now punky only exposes providers

--- test/integration/service-factory-test.js
+++ test/integration/service-factory-test.js
@@ -1,18 +1,12 @@
 'use strict'
 
 const defaultOptions = require(__source + 'config/default')
-const LoggerFactory = require(__source + 'logger/logger-factory')
-const MetricsFactory = require(__source + 'metrics/metrics-factory')
-const Router = require('express').Router
 const RunnerInterface = require(__source + 'runner-interface')
 const ServiceFactory = require(__source + 'service-factory')
 
 describe('service-factory', () => {
   it('.create() should return a Runner instance', () => {
-    const router = Router()
-    const logger = LoggerFactory.create(defaultOptions)
-    const metrics = MetricsFactory.create(logger, defaultOptions)
-    const service = ServiceFactory.create(router, metrics, logger, defaultOptions)
+    const service = ServiceFactory.create(defaultOptions)
     service.should.be.instanceOf(RunnerInterface)
   })
 })
