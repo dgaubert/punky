a5f19d4f2f9ea6b55abd3aeb795268cede3a671b
Daniel García Aubert <dgaubert@cartodb.com>
2016-09-11 14:39:01 +0200

    Improved app.js for benchmark

--- benchmark/app.js
+++ benchmark/app.js
@@ -6,9 +6,10 @@ const Router = require('express').Router
 const punky = new Punky(/* options */)
 const router = Router()
 const body = new Buffer('Hello World')
+const message = body.toString('utf8')
 
 router.get('/', (req, res, next) => {
-  req.log.info('greting ...')
+  req.log.info(message)
   req.metrics.increment('home')
   res.set('Content-Type', 'text/html')
   res.send(body)
