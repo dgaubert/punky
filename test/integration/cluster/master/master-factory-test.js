c416253d6a47ab94c576fb3670e666ae040ca8f1
Daniel García Aubert <dgaubert@cartodb.com>
2016-09-13 22:04:51 +0200

    Fixed bad parameter provided

--- test/integration/cluster/master/master-factory-test.js
+++ test/integration/cluster/master/master-factory-test.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const assert = require('assert')
+const defaultOptions = require(__source + 'config/default')
 const RunnerInterface = require(__source + 'runner-interface')
+const LoggerFactory = require(__source + 'logger/logger-factory')
 const MasterFactory = require(__source + 'cluster/master/master-factory')
 
 describe('master-factory', () => {
   it('.create() should return a Runner instance', () => {
-    const master = MasterFactory.create()
+    const logger = LoggerFactory.create(defaultOptions)
+    const master = MasterFactory.create(logger)
+
     assert.ok(master instanceof RunnerInterface)
   })
 })
