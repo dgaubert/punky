738ea2b6f4de0616f9d85bb3720978ae29df1e21
Daniel García Aubert <dgaubert@cartodb.com>
2016-10-30 00:14:26 +0200

    Removed service

--- test/integration/launcher/launcher-factory-test.js
+++ test/integration/launcher/launcher-factory-test.js
@@ -3,7 +3,6 @@
 const assert = require('assert')
 const defaultOptions = require(__source + 'config/default')
 const RunnerInterface = require(__source + 'runner-interface')
-const ClusterFactory = require(__source + 'cluster/cluster-factory')
 const LoggerFactory = require(__source + 'logger/logger-factory')
 const MetricsFactory = require(__source + 'metrics/metrics-factory')
 const LauncherFactory = require(__source + 'launcher/launcher-factory')
@@ -12,8 +11,7 @@ describe('launcher-factory', () => {
   it('.create() should return a Runner instance', () => {
     const logger = LoggerFactory.create(defaultOptions)
     const metrics = MetricsFactory.create(logger, defaultOptions)
-    const target = ClusterFactory.create(metrics, logger, defaultOptions)
-    const launcher = LauncherFactory.create(target, logger)
+    const launcher = LauncherFactory.create(metrics, logger, defaultOptions)
 
     assert.ok(launcher instanceof RunnerInterface)
   })
