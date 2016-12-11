6177fc4f7b923b3aee183a3f1d25f9db095d09a1
Daniel García Aubert <dgaubert@cartodb.com>
2016-12-11 19:31:24 +0100

    Remove unused dependency

--- test/integration/punky-test.js
+++ test/integration/punky-test.js
@@ -1,17 +1,13 @@
 'use strict'
 
 const Punky = require(__source + 'punky')
-const Router = require('express').Router
 
 describe('punky', () => {
   beforeEach(() => {
-    this.router = Router()
     this.punky = new Punky()
   })
 
   it('service.run() should init the service', () => {
-    this.punky.app.use(this.router)
-
     return this.punky.run()
       .then(() => this.punky.close())
   })
