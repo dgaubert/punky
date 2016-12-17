872e5b74baf5b31397de3cf7eb8a8fcba81b36bc
Daniel García Aubert <dgaubert@cartodb.com>
2016-12-17 15:29:18 +0100

    Use hello-world example for benchmark

--- benchmark/app.js
+++ benchmark/app.js
@@ -1,32 +1,13 @@
 'use strict'
 
-const Punky = require('../')
-const Router = require('express').Router
+const HelloWorld = require('../example/hello-world')
+const helloWorld = new HelloWorld()
 
-const punky = new Punky(/* { options } */)
-const logger = punky.logger
-const app = punky.app
-
-if (app) {
-  const router = Router()
-  const body = new Buffer('Hello World')
-  const message = body.toString('utf8')
-
-  router.get('/', (req, res, next) => {
-    req.log.info(message)
-    req.metrics.increment('home')
-    res.set('Content-Type', 'text/html')
-    res.send(body)
-  })
-
-  app.use(router)
-}
-
-punky.run()
+helloWorld.punky.run()
   // if process was spawned with IPC channel,
   // then send "listening port" to the parent process
-  .then(server => typeof process.send === 'function'
-    ? process.send({ port: server.address().port })
+  .then(httpServer => typeof process.send === 'function'
+    ? process.send({ port: httpServer.address().port })
     : undefined
   )
-  .catch(err => logger.error(err))
+  .catch(err => helloWorld.punky.logger.error(err))
