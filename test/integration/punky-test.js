e59e73da820a55f75cc025ff90639905fe5f0bb3
Daniel García Aubert <dgaubert@cartodb.com>
2016-12-26 19:15:12 +0100

    Expose role

--- test/integration/punky-test.js
+++ test/integration/punky-test.js
@@ -1,14 +1,20 @@
 'use strict'
 
+const assert = require('assert')
 const Punky = require(__source + 'punky')
+const Role = require(__source + 'cluster/role')
 
 describe('punky', () => {
   beforeEach(() => {
     this.punky = new Punky()
   })
 
-  it('service.run() should init the service', () => {
+  it('.run() should init the service', () => {
     return this.punky.run()
       .then(() => this.punky.close())
+  })
+
+  it(`.role() should return ${Role.SERVER}`, () => {
+    assert.equal(this.punky.role, Role.SERVER)
   })
 })
