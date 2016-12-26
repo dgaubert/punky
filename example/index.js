324e6252e8a65df2795373215b4f025c6460d068
Daniel García Aubert <dgaubert@cartodb.com>
2016-12-26 19:44:32 +0100

    Add script to run any example

---
+++ example/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const meow = require('meow')
+const help = `
+  Usage:
+    $ npm run example [-- <options>]
+
+  Options:
+    -n, --name Launch the given example (default: hello-world)
+
+  Examples
+    $ npm run example -- -n hello-world
+`
+const options = {
+  alias: {
+    n: 'name'
+  },
+  default: {
+    name: 'hello-world'
+  }
+}
+
+const flags = meow({ help }, options).flags
+const exampleName = flags.name
+const Example = require(`./${exampleName}`)
+const example = new Example()
+
+example.punky.run()
+  .catch(err => example.punky.logger.error(err))
