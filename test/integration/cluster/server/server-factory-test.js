950d4ff89a6eea3885fbb06776d92b4b932b9e01
Daniel García Aubert <dgaubert@cartodb.com>
2016-12-31 12:13:50 +0100

    Rename and fix test

--- test/integration/cluster/server/server-factory-test.js
+++ test/integration/cluster/server/server-factory-test.js
@@ -4,16 +4,14 @@ const assert = require('assert')
 const options = require(__lib + 'config/defaults')
 const LoggerFactory = require(__lib + 'logger/logger-factory')
 const MetricsFactory = require(__lib + 'metrics/metrics-factory')
-const Router = require('express').Router
 const RunnerInterface = require(__lib + 'runner-interface')
 const ServerFactory = require(__lib + 'cluster/server/server-factory')
 
-describe('worker-factory', () => {
+describe('server-factory', () => {
   it('.create() should return a Runner instance', () => {
-    const router = Router()
     const logger = LoggerFactory.create(options)
     const metrics = MetricsFactory.create(logger, options)
-    const server = ServerFactory.create(router, metrics, logger, options)
+    const server = ServerFactory.create(metrics, logger, options)
 
     assert.ok(server instanceof RunnerInterface)
   })
