28e614f442fc2af145a0a6af063cf7203d9aafdd
Daniel García Aubert <dgaubert@cartodb.com>
2017-01-06 10:56:49 +0100

    Add unit test for middleware interface

---
+++ test/unit/http-server/app/middleware/middleware-interface-test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const assert = require('assert')
+const MiddlewareInterface = require(__lib + 'http-server/app/middleware/middleware-interface')
+
+class Middleware extends MiddlewareInterface {}
+
+describe('metrics-interface', () => {
+  beforeEach(() => {
+    this.middleware = new Middleware()
+  })
+  it('create interface directly with "new" should throw error', () => {
+    assert.throws(() => new MiddlewareInterface(), 'MiddlewareInterface cannot be directly constructed')
+  })
+
+  it('.regist() should throw "Unimplemented method" error', () => {
+    assert.throws(() => this.middleware.regist(), 'Unimplemented method')
+  })
+})
