525887524faeea69fee1396314f1798f49f7eccb
Daniel García Aubert <dgaubert@cartodb.com>
2017-01-14 14:07:09 +0100

    Improved test coverage for logger

---
+++ test/unit/logger/log-command-listener-test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const assert = require('assert')
+const sinon = require('sinon')
+const EventEmitter = require('events')
+const ListenerInterface = require(__lib + 'listener-interface')
+const LogCommandListener = require(__lib + 'logger/log-command-listener')
+
+describe('log-command-listener', () => {
+  beforeEach(() => {
+    this.sandbox = sinon.sandbox.create()
+
+    this.emitter = new EventEmitter()
+    this.logCommandListener = new LogCommandListener(this.emitter)
+  })
+
+  afterEach(() => {
+    this.sandbox.restore()
+  })
+
+  it('should be an instace of ListenerInterface', () => {
+    assert.ok(this.logCommandListener instanceof ListenerInterface)
+  })
+
+  it('.listen() should attach listener to message process event', () => {
+    var reopenFileStreamsStub = this.sandbox.stub()
+
+    this.logCommandListener.listen(reopenFileStreamsStub)
+    this.emitter.emit('message', 'logger:reopen-file-streams')
+
+    assert.ok(reopenFileStreamsStub.calledOnce)
+  })
+})
